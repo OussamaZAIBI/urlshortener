c53750c29c06b49c42ada63247d9ef512f2b2ba7
Oussama ZAIBI <oussama.zaibi@supcom.tn>
2023-01-05 20:40:51 +0100

    step1

--- urlshortener_app/config.py
+++ urlshortener_app/config.py
@@ -1,5 +1,6 @@
 
 from pydantic import BaseSettings
+from functools import lru_cache
 
 
 class Settings(BaseSettings):
@@ -9,8 +10,11 @@ class Settings(BaseSettings):
     base_url: str = "http://localhost:8000"
 
     db_url: str = "sqlite:///./shortener.db"
+    class Config:
+        env_file = ".env"
 
 
+@lru_cache
 def get_settings() -> Settings:
 
     settings = Settings()
